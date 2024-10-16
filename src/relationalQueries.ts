59cf7f57484b51102ca59ebaab50ab0537872403
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2024-10-16 16:35:52 +0600

    user post all see

--- src/relationalQueries.ts
+++ src/relationalQueries.ts
@@ -3,11 +3,11 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const relationalQueries = async () => {
-    const result = await prisma.user.findUnique({
-        where: {
-            id: 1
-        }
-    }).Post() // fluent api call > just user post show
+    // const result = await prisma.user.findUnique({
+    //     where: {
+    //         id: 1
+    //     }
+    // }).Post() // fluent api call > just user post show
     // user ar  post and profile include diye ber kora
     // const result = await prisma.user.findUnique({
     //     where: {
@@ -18,7 +18,19 @@ const relationalQueries = async () => {
     //         profile: true
     //     }
     // })
-    console.log(result)
+    const publishedPostUser = await prisma.user.findMany({
+        where: {
+            id: 1
+        },
+        include: {
+            Post: {
+                where: {
+                    published: false
+                }
+            }
+        }
+    })
+    console.dir(publishedPostUser, { depth: Infinity })
 }
 
 relationalQueries()
